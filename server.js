3499f2332ded079befd207d4e4470d9b7bea3831
DevGupta <devgupta851900@gmail.com>
2024-08-28 22:27:12 +0530

    modified cors configuration

--- server.js
+++ server.js
@@ -22,6 +22,8 @@ app.use(
 			"https://kudosware-seven.vercel.app/",
 		],
 		credentials: true,
+		methods: ["GET", "POST", "PUT", "DELETE"],
+		allowedHeaders: ["Content-Type", "Authorization"],
 	})
 );
 app.use(
